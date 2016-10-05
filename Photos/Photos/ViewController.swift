5337de34b6bf07afa57fe38eb74c59745a982046
ezefranca <ezequiel.ifsp@gmail.com>
2016-10-05 14:01:58 -0300

    initial project

---
+++ Photos/Photos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by techthings on 10/5/16.
+//  Copyright © 2016 Ezequiel França. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
